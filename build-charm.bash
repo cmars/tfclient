4c38b9e655bef9c7e0ec5416e0be7ba0c70fa451
Casey Marshall <casey.marshall@canonical.com>
2016-10-06 17:58:13 -0500

    Support upgrade-charm, samples, misc fixes.

--- build-charm.bash
+++ build-charm.bash
@@ -4,9 +4,10 @@ set -ex
 
 HERE=$(cd $(dirname $0); pwd)
 
-mkdir -p ${HERE}/layers/tfdemo/bin
+mkdir -p ${HERE}/layers/tfdemo/bin ${HERE}/layers/tfdemo/samples
 
 GOBIN=${HERE}/layers/tfdemo/bin go install ./cmd/tfwebapp
+cp -r samples/* ${HERE}/layers/tfdemo/samples
 
 JUJU_REPOSITORY=${HERE} LAYER_PATH=${HERE}/layers INTERFACE_PATH=${HERE}/interfaces \
 	charm build ${HERE}/layers/tfdemo
--- layers/tfdemo/reactive/tfdemo.py
+++ layers/tfdemo/reactive/tfdemo.py
@@ -1,17 +1,25 @@
 import os
 import shutil
+from subprocess import check_call
 
-from charms.reactive import when, when_not, set_state, remove_state
+from charms.reactive import when, when_not, set_state, remove_state, hook
 from charms.reactive.helpers import data_changed
 
 from charmhelpers.core.templating import render
 from charmhelpers.core import hookenv, host
 
 
+@hook('upgrade-charm')
+def upgrade_charm():
+    install_tfdemo()
+
+
 @when_not('tfdemo.installed')
 def install_tfdemo():
     shutil.copyfile("bin/tfwebapp", "/usr/bin/tfwebapp")
     os.chmod("/usr/bin/tfwebapp", 0o755)
+    if host.service_available('tfdemo') and host.service_running('tfdemo'):
+        host.service_restart('tfdemo')
     set_state('tfdemo.installed')
 
 
@@ -23,17 +31,24 @@ def tensorflow_available(tf):
             host.service_stop('tfdemo')
         return
     hookenv.open_port(8080)
+    ctx = {
+        'addr': ','.join(addrs),
+    }
+    samples_dir = os.path.join(hookenv.charm_dir(), "samples")
+    if os.path.exists(samples_dir):
+        ctx['samples'] = samples_dir
     render(source="tfdemo.service",
         target="/etc/systemd/system/tfdemo.service",
         owner="root",
         perms=0o644,
-        context={
-            'addr': ','.join(addrs),
-        })
+        context=ctx,
+    )
+    check_call(['systemctl', 'daemon-reload'])
     if host.service_running('tfdemo'):
         host.service_restart('tfdemo')
     else:
         host.service_start('tfdemo')
+    remove_state('client.available')
 
 
 @when('website.available')
